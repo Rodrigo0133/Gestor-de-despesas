1596ea00e78d3ccf548b153cf0fc4c47c4cea486
Ghxst <rodrigo.galante@sapo.pt>
2026-08-30 18:48:40 +0100

    melhoração na gui do register

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,8 +1,49 @@
+import { KeyRound, Mail, User } from "lucide-react";
+
 function Login() {
-    return(
-        <div>
-            
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center bg-surface text-primary">
+      <div className="flex flex-col items-center justify-center gap-7 rounded-md border border-primary/20 bg-surface p-6 shadow-lg">
+        <h1 className="text-2xl">Registrar</h1>
+
+        <div className="flex">
+          <label htmlFor="nome" className="mr-2 translate-y-2.5">
+            <User />
+          </label>
+          <input
+            id="nome"
+            placeholder="Digite o seu nome"
+            className="w-full border-primary/20 px-3 py-2 rounded-md focus:border-primary focus:ring-2"
+          />
         </div>
-    )
+        <div className="flex">
+          <label htmlFor="email" className="mr-2 translate-y-2.5">
+            <Mail />
+          </label>
+          <input
+            id="email"
+            placeholder="Digite o seu email"
+            className="w-full border-primary/20 px-3 py-2 rounded-md focus:border-primary focus:ring-2"
+          />
+        </div>
+        <div className="flex">
+          <label htmlFor="senha" className="mr-2 translate-y-2.5">
+            <KeyRound />
+          </label>
+          <input
+            id="senha"
+            placeholder="Digite o seu email"
+            className="w-full border-primary/20 px-3 py-2 rounded-md focus:border-primary focus:ring-2"
+          />
+        </div>
+        <button
+          type="submit"
+          className="cursor-pointer rounded-md bg-primary px-4 py-2 font-medium text-surface transition hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-primary/40 active:scale-95"
+        >
+          Registar
+        </button>
+      </div>
+    </div>
+  );
 }
-export default Login
+export default Login;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router";
+import './style.css'
 import App from './App.tsx'
 import Login from "./Login.tsx";
 
